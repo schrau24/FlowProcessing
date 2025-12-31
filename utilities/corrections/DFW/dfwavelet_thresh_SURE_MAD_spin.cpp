2637a770a4877b3c11e351ac5cd6a9ddc8ec2f41
Schrauben <e.m.schrauben@amsterdamumc.nl>
2025-12-31 11:30:29 +0100

    added DFW files for compiling

--- utilities/corrections/DFW/dfwavelet_thresh_SURE_MAD_spin.cpp
+++ utilities/corrections/DFW/dfwavelet_thresh_SURE_MAD_spin.cpp
@@ -4,7 +4,6 @@
 #include <stdio.h>
 
 /* Input Arguments */
-
 #define	VX_IN	prhs[0]
 #define	VY_IN	prhs[1]
 #define	VZ_IN	prhs[2]
@@ -14,67 +13,98 @@
 #define ISRAND_IN prhs[6]
 
 /* Output Arguments */
-
 #define	VX_OUT	plhs[0]
 #define	VY_OUT	plhs[1]
 #define	VZ_OUT	plhs[2]
 
 void mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[])
 {
-  double *in_vx,*in_vy,*in_vz, *out_vx,*out_vy,*out_vz, *minSizeDouble,*res;
-  int * dimsint;
-  int *dims,*minSize;
+    double *in_vx, *in_vy, *in_vz;
+    double *out_vx, *out_vy, *out_vz;
+    double *minSizeDouble, *res;
+
+    const mwSize *dims_mw;   // MATLAB dims
+    mwSize *dims_mw_copy;    // local copy (mwSize)
+    mwSize *minSize_mw;      // minSize as mwSize for MATLAB
+
+    int *dims_int;           // dims passed to dfwavelet (int)
+    int *minSize_int;        // minSize passed to dfwavelet (int)
+
   int spins,isRand;
+    int i;
   
-  /* Check for proper number of arguments */
-  
+    /* Check number of inputs */
   if (nrhs !=7) {
     mexErrMsgTxt("dfwavelet_thresh requires 7 input arguments.");
   }
 
-  if (mxIsComplex(VX_IN))
-    mexErrMsgTxt ("Input must be real");
+    /* Check that VX is real */
+    if (mxIsComplex(VX_IN)) {
+        mexErrMsgTxt("Input must be real.");
+    }
   
-  
-  // Assign pointers to the various parameters
+    /* --- Get pointers to input data --- */
   in_vx = (double*) mxGetData(VX_IN);
   in_vy = (double*) mxGetData(VY_IN);
   in_vz = (double*) mxGetData(VZ_IN);
   res = (double*) mxGetData(RES_IN);
   minSizeDouble = (double*) mxGetData(MINSIZE_IN);
-  minSize = (int*) malloc(sizeof(int)*3);
-  dimsint = (int*) mxGetDimensions(VX_IN);
-  dims = (int*) malloc(sizeof(int)*3);
-  int i;
-  for (i = 0; i<3; i++)
-    {
-      minSize[i] = (int) (minSizeDouble[i]);
-      dims[i] = (int) (dimsint[i]);
+
+    /* --- Get MATLAB dimensions --- */
+    dims_mw = mxGetDimensions(VX_IN);
+
+    /* --- Allocate dimension arrays --- */
+    dims_mw_copy = (mwSize*) mxMalloc(3 * sizeof(mwSize));
+    minSize_mw   = (mwSize*) mxMalloc(3 * sizeof(mwSize));
+
+    dims_int     = (int*) mxMalloc(3 * sizeof(int));
+    minSize_int  = (int*) mxMalloc(3 * sizeof(int));
+
+    /* --- Convert dims and minSize --- */
+    for (i = 0; i < 3; i++) {
+        dims_mw_copy[i] = dims_mw[i];
+        minSize_mw[i]   = (mwSize) minSizeDouble[i];
+
+        dims_int[i]     = (int) dims_mw_copy[i];
+        minSize_int[i]  = (int) minSize_mw[i];
     }
   
-  // Create a matrix for the return argument 
-  VX_OUT = mxCreateNumericArray(3, dimsint, mxDOUBLE_CLASS, mxREAL);
-  VY_OUT = mxCreateNumericArray(3, dimsint, mxDOUBLE_CLASS, mxREAL);
-  VZ_OUT = mxCreateNumericArray(3, dimsint, mxDOUBLE_CLASS, mxREAL);
+    /* --- Create output arrays --- */
+    VX_OUT = mxCreateNumericArray(3, dims_mw, mxDOUBLE_CLASS, mxREAL);
+    VY_OUT = mxCreateNumericArray(3, dims_mw, mxDOUBLE_CLASS, mxREAL);
+    VZ_OUT = mxCreateNumericArray(3, dims_mw, mxDOUBLE_CLASS, mxREAL);
 
   out_vx = mxGetPr(VX_OUT);
   out_vy = mxGetPr(VY_OUT);
   out_vz = mxGetPr(VZ_OUT);
 
+    /* --- Scalars --- */
   spins = (int) mxGetPr(SPINS_IN)[0];
   isRand = (int) mxGetPr(ISRAND_IN)[0];
 
+    /* --- GPU Flag --- */
     int use_gpu = 0;
 #ifdef USE_CUDA
   use_gpu = 2;
 #endif
   
-  // Do the actual computations in a subroutine
-  struct dfwavelet_plan_s* plan = prepare_dfwavelet_plan(3,dims,minSize,res,use_gpu);
-  dfwavelet_thresh_SURE_MAD_spin(plan,spins,isRand,out_vx,out_vy,out_vz,in_vx,in_vy,in_vz);
+    /* --- Prepare wavelet plan --- */
+    struct dfwavelet_plan_s* plan =
+        prepare_dfwavelet_plan(3, dims_int, minSize_int, res, use_gpu);
+
+    /* --- Run transform --- */
+    dfwavelet_thresh_SURE_MAD_spin(
+        plan, spins, isRand,
+        out_vx, out_vy, out_vz,
+        in_vx,  in_vy,  in_vz
+    );
   
+    /* --- Cleanup --- */
   dfwavelet_free(plan);
-  free(dims);
-  free(minSize);
-  return;
+
+    mxFree(dims_mw_copy);
+    mxFree(minSize_mw);
+
+    mxFree(dims_int);
+    mxFree(minSize_int);
 }
---
+++ utilities/corrections/DFW/mexMake.m
@@ -0,0 +1,2 @@
+% Make file for mex functions
+mex -I./src ./dfwavelet_thresh_SURE_MAD_spin.cpp ./src/dfwavelet.cpp
